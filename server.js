4a2ec7417ee6492f9f2a2fcabdfeff4deeced872
christylaguardia <christinelaguardia@gmail.com>
2017-08-28 11:14:07 -0700

    added get images route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const http = require('http');
 const app = require('./lib/app');
-const connect = require('./lib/connect');s
+const connect = require('./lib/connect');
 
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/emoji';
 connect(dbUri);
